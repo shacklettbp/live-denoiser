fix bug when dumping inference data, change temporal gradients to noise2nosie loss

trainlive.py
@@ -157,7 +157,7 @@ def train(state, color, normal, albedo, alt_color, alt_color2, alt_color3, alt_a
         else:
             save_indices = np.random.permutation(state.args.num_crops)
 
-        state.prev_crops = train_crops[save_indices]
+        #state.prev_crops = train_crops[save_indices]
 
         color_train = train_crops[:, 0:3, ...]
         normal_train = train_crops[:, 3:5, ... ]
@@ -184,7 +184,7 @@ def train(state, color, normal, albedo, alt_color, alt_color2, alt_color3, alt_a
             total_loss += loss
             loss.backward()
             state.optimizer.step()
-            state.scheduler.batch_step()
+            #state.scheduler.batch_step()
 
     #state.scheduler.step()
     print(float(loss.cpu()) / state.args.outer_train_iters)
@@ -204,7 +204,7 @@ def create_model(args, dev, weights):
 
 def init_training_state(dev=torch.device('cuda:{}'.format(0)), init_weights=None):
     #args = Args(lr=0.001, outer_train_iters=1, inner_train_iters=1, num_crops=32, cropsize=64, augment=False, importance_sample=False)
-    args = Args(lr=0.00003, outer_train_iters=128, inner_train_iters=1, num_crops=8, cropsize=128, augment=False, importance_sample=False)
+    args = Args(lr=0.000003, outer_train_iters=32, inner_train_iters=1, num_crops=8, cropsize=128, augment=False, importance_sample=False)
     model = create_model(args, dev, init_weights)
     #for name, param in model.named_parameters():
     #    if not name.startswith("model.kernel"):
@@ -229,7 +229,8 @@ def init_training_state(dev=torch.device('cuda:{}'.format(0)), init_weights=None
             return 5e-5
 
     #scheduler = LambdaLR(optimizer, lr_lambda=schedule_func)
-    scheduler = CyclicLR(optimizer, args.lr / 10, args.lr, step_size=50)
+    #scheduler = CyclicLR(optimizer, args.lr / 10, args.lr, step_size=50)
+    scheduler = None
 
     return TrainingState(model, optimizer, scheduler, loss_gen, args)
 