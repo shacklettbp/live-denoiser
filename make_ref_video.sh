5a5c35d6840b7bc5ca06f6c1a9520d925fc65894
Brennan Shacklett <bpshacklett@gmail.com>
2019-05-19 18:48:42 -0700

    videos for metrics should be stored rgb444

--- make_ref_video.sh
+++ make_ref_video.sh
@@ -6,4 +6,4 @@ else
   prefix="$3"
 fi
 
-ffmpeg -ss 0 -t 50 -apply_trc gamma28 -i $1/${prefix}_%d.exr -pix_fmt yuv420p -c:v libx264 -crf 0 $2
+ffmpeg -ss 0 -t 50 -apply_trc gamma28 -i $1/${prefix}_%d.exr -pix_fmt bgr24 -c:v libx264rgb -crf 0 $2
